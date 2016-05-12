7b36e1695d089e44f0fed70b1bce9a0937612aa4
zachnewburgh <zach.newburgh@gmail.com>
2016-05-12 14:56:47 -0400

    created private #user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,18 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params[:user])
+    @user = User.new(user_params)
     @user.save
     redirect_to @user
   end
 
   def show
+    @user = User.find(params[:id])
   end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :height, :happiness, :nausea, :tickets)
+  end
+
 end
